0b1391aa6ff0334bc8d9e84b901df98f8f0e614f
Aimes <na.aguilera@hotmail.com>
2019-02-14 19:35:32 -0600

    add new pages

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+
+// function hoverOver(x) {
+//     x.style.height = "350px";
+// }
+
+$("#block-one").on("click", function(){
+    $("#my-info").append("<div class='container' id='about-me'</div>");
+    // $("#about-me").html("<div class='container'>");
+})
+
+
