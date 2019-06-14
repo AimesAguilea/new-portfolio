2d57704fe7e6562bacd5738773a68430da13bde8
Aimes <na.aguilera@hotmail.com>
2019-06-14 17:36:41 -0500

    some code cleanup

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,4 @@
 
-// function hoverOver(x) {
-//     x.style.height = "350px";
-// }
 
 $("#block-one").on("click", function(){
     $("#my-info").append("<div class='container' id='about-me'</div>");
